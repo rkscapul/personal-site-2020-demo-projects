9e6b8be2b9a4a9a67a3d0119de81e305c52c7e8e
Ryle Capul <kencap119@gmail.com>
2020-07-21 20:09:09 +0800

    initial commit

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <img class="site-logo" alt="logo" src="../assets/icon.png" />
+  </div>
+</template>
+
+<style>
+  .header {
+    text-align: center;
+  }
+
+  .header .site-logo {
+    height: 100px;
+    width: 100px;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'HeaderComponent'
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      <HeaderComponent class="header-template" />
+      <h1 class="title">
+        personal-site-2020-demo
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+  .header-template {
+    padding-top: 50px;
+  }
+</style>
